0bb7720799eaad7d90d5e46d7ddb9f0a8452b80a
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-23 14:56:02 +0530

    Refactor Contact page to add dark mode support and update background color

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import ContactUs from '@/components/ContactUs';
 
 const FullScreenImages = () => {
   return (
-    <div className="bg-black w-full pt-[80px]">
+    <div className="bg-light-purple dark:bg-black w-full pt-[80px]">
       <ContactBackground />
       <ContactUs />
       <ThankYou />
