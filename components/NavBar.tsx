b7c58cd8dff93f46da558fc53a1f368417a9530b
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-23 14:22:36 +0530

    Refactor NavBar component to add dark mode support and toggle button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,23 +1,49 @@
 'use client';
-import React from 'react';
-import GlassMorphism from './ui/GlassMorphism';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { NavLinks } from '@/constants/Navbar';
 import { Logo } from '@/constants/Icons';
 import { FiMenu } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
+import { HiOutlineSun, HiOutlineMoon } from 'react-icons/hi'; // Stylish icons
+import React, { useState, useEffect } from 'react';
 
 function NavBar() {
   const path = usePathname();
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Dark mode state
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'dark') {
+      setDarkMode(true);
+      document.documentElement.classList.add('dark');
+    } else {
+      setDarkMode(false);
+      document.documentElement.classList.remove('dark');
+    }
+  }, []);
+
+  // Toggle dark mode in localStorage
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  };
 
   return (
-    <div className="h-20 md:h-fit fixed top-0 left-0 right-0 z-50 flex items-center justify-center pt-8 md:pt-0">
-      <div className="w-full px-4 md:p-4">
-        <div className={`w-full h-fit ${isOpen && 'mt-60'} md:mt-0`}>
+    <div className="h-20 md:h-fit fixed top-0 left-0 right-0 z-50 flex items-center justify-center pt-8 md:pt-0 ">
+      <div className="w-full px-4 md:p-4 ">
+        <div className={`w-full h-fit dark:shadow-none dark:backdrop-blur-0 shadow-[4px_4px_4px_rgba(0,0,0,0.25)] backdrop-blur-[16px] ${isOpen && 'mt-60'} md:mt-0`}>
           <div
-            className={`text-white rounded-lg md:px-8 min-h-16 md:h-20 w-full p-2 pt-3 bg-[#282828]
+            className={`dark:text-white text-black rounded-lg md:px-8 min-h-16 md:h-20 w-full p-2 pt-3 dark:bg-[#282828] bg-white
             ${!isOpen ? 'flex items-center' : ''}`}
           >
             <div className="flex items-center justify-between w-full relative">
@@ -29,7 +55,6 @@ function NavBar() {
                   <span>Anonymous</span>
                   <span>Squirrels</span>
                 </div>
-
                 <div className="h-fit w-fit flex md:hidden element z-20 -ml-5 px-5 ">
                   <Logo className="h-8" />
                 </div>
@@ -45,7 +70,7 @@ function NavBar() {
                     key={item.tag}
                     className={`${
                       path === item.path &&
-                      'text-sq-violet group-hover:text-white'
+                      'text-sq-violet group-hover:text-black dark:group-hover:text-white'
                     }
              hover:text-sq-violet  transition-all duration-300`}
                   >
@@ -76,6 +101,23 @@ function NavBar() {
                 ))}
               </div>
             )}
+            {/* dark toggle button  */}
+            <button
+            title='toggle'
+              onClick={toggleDarkMode}
+              className="mx-4 p-2 rounded-full transition-all duration-500"
+            >
+              <div className="relative h-8 w-8">
+                <HiOutlineSun
+                  className={`absolute h-full w-full transition-transform duration-500 ${darkMode ? 'rotate-180 scale-0' : 'scale-100'
+                    }`}
+                />
+                <HiOutlineMoon
+                  className={`absolute h-full w-full text-gray-300 transition-transform duration-500 ${darkMode ? 'scale-100' : 'scale-0 rotate-180'
+                    }`}
+                />
+              </div>
+            </button>
           </div>
         </div>
       </div>
