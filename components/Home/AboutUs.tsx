91f2815ed137b756a2deafd2eb7d63274d2d8c20
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-23 16:15:22 +0530

    Refactor AboutUs component to update dark mode styling and text color

--- components/Home/AboutUs.tsx
+++ components/Home/AboutUs.tsx
@@ -8,7 +8,7 @@ function AboutUs() {
   return (
     <div id={'about-us'} className=" dark:bg-black bg-light-purple text-black dark:text-white">
       <div className="flex pt-7 px-7 p-7 md:p-14 md:pt-10">
-        <span className="uppercase text-5xl md:text-8xl font-bold mx-auto  bg-gradient-to-r from-[#A998FF] via-[#7B61FF] to-[#5237D8] inline-block text-transparent bg-clip-text">
+        <span className="uppercase text-5xl md:text-8xl font-bold mx-auto  bg-gradient-to-r dark:from-[#A998FF] from-[#5C3095] dark:via-[#7B61FF] via-[#6D5ACB] dark:to-[#5237D8] to-[#5237D8] inline-block text-transparent bg-clip-text">
           About Us
         </span>
       </div>
@@ -17,7 +17,7 @@ function AboutUs() {
         <div className="flex flex-col items-center justify-between gap-y-10">
           <div className="text-center text-xl md:text-2xl px-8 md:px-0 font-bold max-w-[900px] italic">
             We are a team of{' '}
-            <span className="text-[#9C3DFC] italic">
+            <span className="dark:text-[#9C3DFC] text-dark-purple italic">
               storytellers, creative wizards and tech geeks
             </span>{' '}
             {/* who help you convey your story better. */}
