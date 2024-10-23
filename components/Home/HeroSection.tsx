28c85d10e3a1cd915fb1101587d2b4a2797c67ff
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-23 14:23:53 +0530

    Refactor HeroSection component to add dark mode support and update colors

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -14,7 +14,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="relative h-fit bg-black pb-7 md:pb-10 w-full overflow-hidden pt-16 md:pt-0">
+    <div className="relative h-fit dark:bg-black bg-light-purple pb-7 md:pb-10 w-full overflow-hidden pt-16 md:pt-0">
       <div className="h-[15vh]"></div>
       <div className="absolute hidden md:block md:left-60 -translate-x-1/2 -top-96 md:-top-72 z-10">
         <Blob variant="violet" />
@@ -28,7 +28,7 @@ export default function Home() {
 
       <div className="relative z-20 flex flex-col md:flex-row items-center justify-between md:mx-10 xl:mx-32 py-4 md:py-8 gap-y-14 md:gap-y-0 mx-0 ">
         <div className="w-10/12 px-2.5 md:w-1/2 order-last md:order-first">
-          <div className="w-full text-white font-semibold text-3xl lg:text-6xl">
+          <div className="w-full dark:text-white text-black font-semibold text-3xl lg:text-6xl">
             {/* <span>Unlock your</span>{" "}
             <span className="text-sq-violet">Brand&apos;s Potential</span>.{" "}
             <span className="hidden md:inline-flex">One at a time Promote and</span>{" "}
@@ -39,7 +39,7 @@ export default function Home() {
             <span className="text-sq-violet">dental marketing </span>
             <span>with our expert team.</span>
           </div>
-          <div className="text-white text-lg md:text-2xl my-5">
+          <div className="dark:text-white text-black text-lg md:text-2xl my-5">
             {/* Elevate your content marketing with a powerful brand strategy. */}
             We specialize in creating tailored strategies that drive results and
             help you stand out in your industry.
@@ -58,29 +58,53 @@ export default function Home() {
 
         <div className="relative order-first md:order-last">
           <Squirrel className="lg:h-[500px] lg:w-[420px]" />
+ {/* dark theme */}
           <GlassMorphism
             variant="light"
-            className="rounded-xl inline-block absolute -top-2 md:top-0 right-0 translate-x-1/2"
+            className="rounded-xl dark:inline-block hidden absolute -top-2 md:top-0 right-0 translate-x-1/2"
           >
             <Laptop className="h-7 w-7 md:h-fit md:w-fit" />
           </GlassMorphism>
+{/* light theme */}
+          <GlassMorphism
+            variant="dark"
+            className="rounded-xl dark:hidden inline-block absolute -top-2 md:top-0 right-0 translate-x-1/2"
+          >
+            <Laptop className="h-7 w-7 md:h-fit md:w-fit" />
+
+          </GlassMorphism>
+{/* dark theme */}
           <GlassMorphism
             variant="light"
-            className="rounded-xl inline-block absolute -bottom-2 -right-6 md:bottom-0 md:right-0 -translate-x-1/2"
+            className="rounded-xl dark:inline-block hidden absolute -bottom-2 -right-6 md:bottom-0 md:right-0 -translate-x-1/2"
           >
             <Brush className="h-7 w-7 md:h-fit md:w-fit" />
           </GlassMorphism>
-
+      {/* light theme */}
+          <GlassMorphism
+            variant="dark"
+            className="rounded-xl dark:hidden inline-block absolute -bottom-2 -right-6 md:bottom-0 md:right-0 -translate-x-1/2"
+          >
+            <Brush className="h-7 w-7 md:h-fit md:w-fit" />
+          </GlassMorphism>
+     {/* dark theme    */}
           <GlassMorphism
             variant="light"
-            className="rounded-xl inline-block absolute -left-3 top-[40%] md:left-0 md:top-1/2 -translate-y-1/2"
+            className="rounded-xl dark:inline-block hidden absolute -left-3 top-[40%] md:left-0 md:top-1/2 -translate-y-1/2"
+          >
+            <PenRuler className="h-7 w-7 md:h-fit md:w-fit" />
+          </GlassMorphism>
+       {/* light  theme */}
+          <GlassMorphism
+            variant="dark"
+            className="rounded-xl dark:hidden inline-block absolute -left-3 top-[40%] md:left-0 md:top-1/2 -translate-y-1/2"
           >
             <PenRuler className="h-7 w-7 md:h-fit md:w-fit" />
           </GlassMorphism>
         </div>
       </div>
 
-      <div className="text-white w-full flex flex-col justify-center items-center my-8">
+      <div className="dark:text-white text-dark-purple font-bold w-full flex flex-col justify-center items-center my-8">
         <span className="text-2xl font-semibold">Trusted By</span>
         <div className="w-11/12 grid grid-cols-4 lg:grid-cols-8 place-items-center gap-y-4 gap-x-4 md:gap-10 mt-10">
           {Trustee.map(
@@ -100,7 +124,9 @@ export default function Home() {
                   />
                 </div>
 
-                <p className="my-2.5 text-xs md:text-base max-w-[90px] md:max-w-fit">{item.name}</p>
+                <p className="my-2.5 text-xs md:text-base max-w-[90px] md:max-w-fit">
+                  {item.name}
+                </p>
               </div>
             )
           )}
