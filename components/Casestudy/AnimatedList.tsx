f20f31f2141c6f61eb1a57cacf4fe712e50f1b8c
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-23 14:23:08 +0530

    Refactor AnimatedList component to update styles for dark mode support

--- components/Casestudy/AnimatedList.tsx
+++ components/Casestudy/AnimatedList.tsx
@@ -22,7 +22,7 @@ const Notification = ({ icon, data, detail }: List) => {
         // light styles
         'bg-white [box-shadow:0_4px_5px_3px_rgba(157,141,255),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
         // dark styles0px 0px 5px 5px rgb(211,177,255)
-        'transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]'
+        'transform-gpu bg-white backdrop-blur-md [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_4px_5px_3px_rgba(157,141,255),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]'
       )}
     >
       <div className="flex flex-row items-center gap-3 min-h-16">
@@ -37,7 +37,7 @@ const Notification = ({ icon, data, detail }: List) => {
           <span className="text-xl md:text-2xl text-[#5C3095] font-semibold">
             {data}{' '}
           </span>
-          <span className="text-xs md:text-sm font-normal dark:text-white/60">
+          <span className="text-xs md:text-sm font-normal text-black">
             {detail}
           </span>
         </div>
