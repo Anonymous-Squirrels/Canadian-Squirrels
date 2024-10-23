4c1efc7e0e45d6acfecd54d0ad708a8327abf54e
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-23 14:22:57 +0530

    Refactor Google component to add dark mode support and update colors

--- components/About/Google.tsx
+++ components/About/Google.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Google = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-10 md:gap-20 justify-center items-center p-5 bg-black">
+    <div className="flex flex-col md:flex-row gap-10 md:gap-20 justify-center items-center p-5 bg-light-purple dark:bg-black">
       <div className="flex-shrink-0">
         <Image
           src="/assets/Google.jpg"
@@ -12,8 +12,8 @@ const Google = () => {
           className="h-[150px] w-[150px] md:h-[350px] md:w-[350px] rounded-xl object-cover"
         />
       </div>
-      <div className="text-white max-w-[900px] flex flex-col gap-5 text-center md:text-left">
-        <p className="text-3xl text-gray-400 font-bold">
+      <div className="dark:text-white text-black max-w-[900px] flex flex-col gap-5 text-center md:text-left">
+        <p className="text-3xl dark:text-gray-400 font-bold text-black">
           We are a Google Partner 2024
         </p>
         <h2 className="text-5xl md:text-6xl text-orange-700 font-bold">
