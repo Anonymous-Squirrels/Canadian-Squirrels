18049110557c0baf1a24e293652ea8bf6becb395
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-25 21:40:15 +0530

    Refactor ProcessCard component to update dark mode styling and text color

--- components/About/ProcessCard.tsx
+++ components/About/ProcessCard.tsx
@@ -14,7 +14,7 @@ const ProcessCard = ({ isHome, logo, heading, about }: Props) => {
       <div
         className={`${
           isHome ? '4xl:h-[319px] 4xl:max-w-[475px] ' : ' h-[120%] shadow-blue'
-        } relative cursor-default duration-[4] transition hover:shadow-custom1 group rounded-[8px] 2xl:rounded-[12px] pb-1 pt-[10.93px] pr-[6.53px] md:pr-[17px] bg-[#111]  border-[#FFC4C4] border gradient-border   hover:shadow-blue  2xl:min-h-[280px] 2xl:max-w-[370px] w-[93%]  md:max-w-[300px] xl:max-w-[300px] md:mt-[60px] 2xl:mt-[122px] sm:mt-[44px] mt-[38px] `}
+        } relative cursor-default duration-[4] transition hover:shadow-custom1 group rounded-[8px] 2xl:rounded-[12px] pb-1 pt-[10.93px] pr-[6.53px] md:pr-[17px] dark:bg-[#111] bg-light-purple  border-[#FFC4C4] border gradient-border   hover:shadow-blue  2xl:min-h-[280px] 2xl:max-w-[370px] w-[93%]  md:max-w-[300px] xl:max-w-[300px] md:mt-[60px] 2xl:mt-[122px] sm:mt-[44px] mt-[38px] `}
       >
         <div
           className={`${
@@ -35,7 +35,7 @@ const ProcessCard = ({ isHome, logo, heading, about }: Props) => {
         <p
           className={`${
             isHome ? 'md:ml-[32px]  4xl:text-xl' : 'mx-5  text-xl '
-          } ml-[9px] sm:ml-[12.92px]  2xl:mt-[46px] mb-1 lg:mt-[44px] md:mt-[35px] sm:mt-[25.83px] mt-[12.35px] text-[#7d8590] duration-500 transition text-base group-hover:text-white 4xl:text-lg leading-[25px]`}
+          } ml-[9px] sm:ml-[12.92px]  2xl:mt-[46px] mb-1 lg:mt-[44px] md:mt-[35px] sm:mt-[25.83px] mt-[12.35px] text-[#7d8590] duration-500 transition text-base group-hover:text-black dark:group-hover:text-white 4xl:text-lg leading-[25px]`}
         >
           {about}
         </p>
